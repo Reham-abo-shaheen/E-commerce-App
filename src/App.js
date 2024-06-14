e76db417497a2a0cba8fe1676fc7749f202ed30a
Reham-abo-shaheen <eng.rehamshaheen@gmail.com>
2024-06-14 19:21:31 +0200

    update E-commerce APP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import ProductsList from "./components/ProductsList";
+import Slider from "./components/Slider";
+import About from "./components/About";
+import { Routes, Route } from "react-router-dom";
+import ProductDetails from "./components/ProductDetails";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<>
+          <Slider />
+          <ProductsList />
+        </>
+        }
+        />
+        <Route path="about" element={<About />} />
+        <Route path="product/:productId" element={<ProductDetails />} />
+      </Routes>;
+
     </div>
   );
 }
---
+++ src/components/Slider.js
@@ -0,0 +1,38 @@
+import ImageOne from '../assets/img-1.jpg';
+import ImageTwo from '../assets/img-2.jpg';
+import ImageThree from '../assets/img-3.webp';
+import './Slider.css'
+function Slider() {
+    return (
+        <>
+            <div id="carouselExampleIndicators" className="carousel slide">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={ImageThree} className="d-block w-100 height-500" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={ImageTwo} className="d-block w-100 height-500" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={ImageOne} className="d-block w-100 height-500" alt="..." />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </>
+    )
+}
+
+export default Slider;
